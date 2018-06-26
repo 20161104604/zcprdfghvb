21ee502bd3785a05515fc2716f7eb26467ee3d93
20161104604 <1045620410@qq.com>
2018-06-26 11:53:48 +0800

    xiugai
    xiugai

--- zcp/main.cpp
+++ zcp/main.cpp
@@ -6,10 +6,21 @@
 //  Copyright © 2018年 20161104604. All rights reserved.
 //
 
-#include <iostream>
+＃include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+struct caipan
+{
+    int mun;
+    double score;
+}cai[7]={{1,1},{2,2},{3,3},{4,4}{5,5},{6,6},{7,7}};
+
+void sort(caipan cai[],int n);
+
+const int N=7;
+
+void mian()
+{
+    cout<<"请输入裁判员的分数\n";
+    cout<<endl;
+    cin>>ping[0].score>>ping[1].score>>
 }
